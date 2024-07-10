b62c8d221e9027a0b11c80feb2a30ece73d10a42
alexandresb <alexandresbriglio@gmail.com>
2024-07-10 17:34:24 +0200

    chap05 : catalog-service passé en Spring boot 3.3.0 persistance & migration v1 avec Flyway

--- src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -2,7 +2,7 @@ package com.polarbookshop.catalogservice.demo;
 
 import com.polarbookshop.catalogservice.domain.Book;
 import com.polarbookshop.catalogservice.domain.BookRepository;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
