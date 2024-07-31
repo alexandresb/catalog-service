2631d9d7f002739c7fa307a7880a9b5347f296ea
alexandresb <alexandresbriglio@gmail.com>
2024-07-31 12:37:39 +0200

    chap06 - containerisation : migration Spring 3.3.2

--- src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
+++ src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
@@ -4,9 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
 @Configuration //source de configurations
-/* activation de l'audit des données lors de leur l'insertion, modification ou suppression d'enregistrement en base
+/* activation de l'audit des données lors de leur insertion, modification ou suppression d'enregistrement en base
 Lors d'une op de persistance, modif ou suppr un event est émis.Spring Data peut capturer cet event et en extraire les métadonnées.
-Ces métadonnées seront intégrées dans les requêtes (cas d'insert ou upadate) au niveau des champs de l'enregistrement correspondant
+Ces métadonnées seront intégrées dans les requêtes (cas d'insert ou update) au niveau des champs de l'enregistrement correspondant
 aux propriétés annotées spécifiquement pour l'audit de l'objet Entité manipulé.
 Si une entité de type record est retournée par la méthode de persistance, le record Java sera mis à jour avec ces valeurs (cela est aussi valable pour delete)
 */
